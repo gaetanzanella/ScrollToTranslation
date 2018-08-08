9b98c6b2fc330e5ca2ac978dabec6293492995e0
Gaétan Zanella <gaetan.zanella@fabernovel.com>
2018-08-08 12:05:07 +0200

    Initial project setup

---
+++ ScrollToTranslation/ScrollToTranslation/ViewController/ViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ViewController.swift
+//  ScrollToTranslation
+//
+//  Created by Gaétan Zanella on 08/08/2018.
+//  Copyright © 2018 Gaétan Zanella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {}
